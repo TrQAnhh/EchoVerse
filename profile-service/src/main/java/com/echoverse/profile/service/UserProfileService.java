de7a8895f32f0f2c22b1365847efc68118afaab9
TrQAnhh <tranqanh2810@gmail.com>
2025-05-06 00:19:29 +0700

    Upload avatar functionality

--- profile-service/src/main/java/com/echoverse/profile/service/UserProfileService.java
+++ profile-service/src/main/java/com/echoverse/profile/service/UserProfileService.java
@@ -1,6 +1,7 @@
 package com.echoverse.profile.service;
 
 import com.echoverse.profile.dto.request.ProfileCreationRequest;
+import com.echoverse.profile.dto.response.ImageFileResponse;
 import com.echoverse.profile.dto.response.UserProfileResponse;
 import com.echoverse.profile.entity.UserProfile;
 import com.echoverse.profile.exception.AppException;
@@ -13,7 +14,9 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -23,6 +26,7 @@ import java.util.List;
 public class UserProfileService {
     UserProfileRepository userProfileRepository;
     UserProfileMapper userProfileMapper;
+    UploadImageService uploadImageService;
 
     public UserProfileResponse createProfile(ProfileCreationRequest request) {
         System.out.println("Received request: " + request);
@@ -57,6 +61,21 @@ public class UserProfileService {
         userProfile = userProfileRepository.save(userProfile);
 
         return userProfileMapper.toUserProfileResponse(userProfile);
+    }
+
+    public ImageFileResponse editUserAvatar(long profileId, MultipartFile file) throws IOException {
+        UserProfile userProfile = userProfileRepository.findById(profileId).orElseThrow(
+                () -> new RuntimeException("Profile not found"));
+
+        var fileUpload = uploadImageService.uploadImage(file);
+
+        userProfile.setAvatar(fileUpload.getUrl());
+        userProfile = userProfileRepository.save(userProfile);
+
+        return ImageFileResponse.builder()
+                .url(fileUpload.getUrl())
+                .originalFileName(fileUpload.getOriginalFileName())
+                .build();
     }
 
     public void deleteProfile(long profileId) {
