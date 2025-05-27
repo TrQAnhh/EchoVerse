6faf408c7e2bb3c7c8b0bf49c570655e162abb00
TrQAnhh <tranqanh2810@gmail.com>
2025-05-27 23:10:14 +0700

    Video service

---
+++ video-service/src/test/java/com/echoverse/video_service/VideoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echoverse.video_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
